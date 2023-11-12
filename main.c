e84439520afb08f39a31ad816ac0bdaa19971d1d
KiPTM <133209150+KiPTM@users.noreply.github.com>
2023-11-12 16:22:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include "command_parser.h"
+#include "requirements.h"
 
 int main(void) {
     char input[1024];
-    char *cmd;
-    char **args = NULL;
 
 
     while (1) {
@@ -19,35 +18,7 @@ int main(void) {
 
         input[strlen(input) - 1] = '\0';
 
-        cmd = input;
-
-        parse_command_with_args(cmd, &cmd, &args);
-
-if (args != NULL) {
-    pid_t child_pid = fork();
-
-    if (child_pid == -1) {
-        perror("fork");
-    } else if (child_pid == 0) {
-        /* Child process */
-        if (execve(cmd, args, NULL) == -1) {
-            perror("execve");
-            _exit(1);
-        }
-    } else {
-        /* Parent process */
-        wait(NULL);
-    }
-}
-
-
-        if (args != NULL) {
-            int i;
-            for (i = 0; args[i] != NULL; i++) {
-                free(args[i]);
-            }
-            free(args);
-        }
+        execute_command(input);
     }
 
     return 0;
