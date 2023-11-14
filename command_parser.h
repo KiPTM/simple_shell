323046c16794dcb60c7aacbd85b2b01585c6aed4
root <KiPTM>
2023-11-14 04:23:50 -0800

    shell update

--- command_parser.h
+++ command_parser.h
@@ -1,9 +1,11 @@
 #ifndef COMMAND_PARSER_H
 #define COMMAND_PARSER_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
-void parse_command_with_args(const char *cmd, char **command, char ***args);
+int  parse_command_with_args(const char *cmd, char **command, char ***args);
 int execute_command_with_args(const char *command, char **args);
+
 
 #endif
